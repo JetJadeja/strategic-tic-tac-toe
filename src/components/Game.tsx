a80976772785f5657c096c82b4e9a36b6152a1e8
JetJadeja <JetJadeja@gmail.com>
2023-10-06 13:28:18 -0700

    Implement Game Shuffling for crazy mode

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,12 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Button } from "@chakra-ui/react";
 import React, { useState } from "react";
 import GameBoard from "./GameBoard";
 
 interface GameProps {
   size: number;
   time: number | null;
+  mode: string;
+  gameMode: string;
 }
 
 // Define an enum for players
@@ -17,6 +19,9 @@ interface TurnIndicatorProps {
   player: "X" | "O";
   isActive: boolean;
   timeLeft: number | null;
+  onShuffle: () => void;
+  hasShuffled: boolean;
+  gameMode: string;
 }
 
 interface LastMoveLocation {
@@ -30,6 +35,9 @@ const TurnIndicator: React.FC<TurnIndicatorProps> = ({
   player,
   isActive,
   timeLeft,
+  onShuffle,
+  hasShuffled,
+  gameMode,
 }) => {
   const color = isActive ? (player === "X" ? "#fc7341" : "#2db2e2") : "#d1d1d1";
   const opacity = isActive ? 1 : 0.5; // 50% opacity for the inactive player
@@ -76,186 +84,244 @@ const TurnIndicator: React.FC<TurnIndicatorProps> = ({
           {formatTime(timeLeft)}
         </Text>
       )}
+      {/* ... (existing children) */}
+      {gameMode === "crazy" && (
+        <button
+          onClick={onShuffle}
+          disabled={hasShuffled}
+          style={{
+            marginTop: "1rem",
+            padding: "0.3rem 0.3rem",
+            backgroundColor: isActive && !hasShuffled ? "#38B2AC" : "#A0AEC0",
+            borderRadius: "8px",
+            cursor: isActive && !hasShuffled ? "pointer" : "not-allowed",
+            transition: "background-color 0.3s, opacity 0.3s",
+          }}
+        >
+          Shuffle Grids
+        </button>
+      )}
     </Box>
   );
 };
 
-const Game: React.FC<GameProps> = React.memo(({ size, time }) => {
-  const initialSquares = Array(size * size)
-    .fill(null)
-    .map(() => Array(size * size).fill(null));
+const Game: React.FC<GameProps> = React.memo(
+  ({ size, time, mode, gameMode }) => {
+    const initialSquares = Array(size * size)
+      .fill(null)
+      .map(() => Array(size * size).fill(null));
 
-  const [squares, setSquares] = useState<typeof initialSquares>(initialSquares);
-  const [localWinners, setLocalWinners] = useState<(string | null)[]>(
-    Array(size * size).fill(null)
-  );
-  const [lastMoveLocation, setLastMoveLocation] = useState<LastMoveLocation>({
-    row: null,
-    col: null,
-    outerRow: null,
-    outerCol: null,
-  });
-  const [xIsNext, setXIsNext] = useState(true);
-  const [winner, setWinner] = useState<string | null>(null);
-
-  const [player1Time, setPlayer1Time] = useState(time);
-  const [player2Time, setPlayer2Time] = useState(time);
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      if (xIsNext) {
-        setPlayer1Time((prev) => (prev ? prev - 1 : null));
-        if (player1Time === 0) setWinner("O");
-      } else {
-        setPlayer2Time((prev) => (prev ? prev - 1 : null));
-        if (player2Time === 0) setWinner("O");
-      }
-    }, 1000);
-
-    // Cleanup the interval when component unmounts or player switches
-    return () => clearInterval(interval);
-  }, [xIsNext]);
-
-  const isCurrentBoard = (idx: number) => {
-    if (winner) return false;
-
-    const { row, col } = lastMoveLocation;
-    if (row === null || col === null) {
-      return true;
-    }
-
-    const currentBoard = row * size + col;
-    return localWinners[currentBoard]
-      ? localWinners[idx] === null
-      : idx === currentBoard;
-  };
-
-  const handleClick = (inner_idx: number, outer_idx: number) => {
-    console.log(inner_idx, outer_idx);
-
-    if (winner || !isCurrentBoard(outer_idx) || squares[outer_idx][inner_idx]) {
-      return;
-    }
-
-    const newSquares = squares.slice();
-    newSquares[outer_idx][inner_idx] = xIsNext ? "X" : "O";
-
-    setSquares(newSquares);
-
-    const lastMove = {
-      row: Math.floor(inner_idx / size),
-      col: inner_idx % size,
-      outerRow: Math.floor(outer_idx / size),
-      outerCol: outer_idx % size,
-    };
-
-    const newWinnerLine = calculateWinner(newSquares[outer_idx], lastMove);
-    const updatedWinners = localWinners.slice();
-    updatedWinners[outer_idx] =
-      newWinnerLine && newSquares[outer_idx][newWinnerLine[0]];
-    setLocalWinners(updatedWinners);
-
-    const globalWinnerLine = calculateWinner(updatedWinners, {
-      row: lastMove.outerRow,
-      col: lastMove.outerCol,
+    const [squares, setSquares] =
+      useState<typeof initialSquares>(initialSquares);
+    const [localWinners, setLocalWinners] = useState<(string | null)[]>(
+      Array(size * size).fill(null)
+    );
+    const [lastMoveLocation, setLastMoveLocation] = useState<LastMoveLocation>({
+      row: null,
+      col: null,
+      outerRow: null,
+      outerCol: null,
     });
-    if (globalWinnerLine) {
-      setWinner(updatedWinners[globalWinnerLine[0]]);
-    }
+    const [xIsNext, setXIsNext] = useState(true);
+    const [winner, setWinner] = useState<string | null>(null);
 
-    setLastMoveLocation(lastMove);
-    setXIsNext(!xIsNext);
-  };
+    const [player1Time, setPlayer1Time] = useState(time);
+    const [player2Time, setPlayer2Time] = useState(time);
 
-  const calculateWinner = (
-    squares: any[],
-    lastMoveLocation: { row: number; col: number }
-  ) => {
-    if (!lastMoveLocation) return null;
+    const [player1HasShuffled, setPlayer1HasShuffled] = useState(false);
+    const [player2HasShuffled, setPlayer2HasShuffled] = useState(false);
 
-    const x = lastMoveLocation.row;
-    const y = lastMoveLocation.col;
-    const lastPlayer = squares[x * size + y];
-    if (lastPlayer === null) return null;
+    React.useEffect(() => {
+      const interval = setInterval(() => {
+        if (xIsNext) {
+          setPlayer1Time((prev) => (prev ? prev - 1 : null));
+          if (player1Time === 0) setWinner("O");
+        } else {
+          setPlayer2Time((prev) => (prev ? prev - 1 : null));
+          if (player2Time === 0) setWinner("O");
+        }
+      }, 1000);
 
-    // Generate possible winner lines for the last move
-    var lines: { [key: string]: number[] } = {
-      row: [],
-      col: [],
-      diag: [],
-      antidiag: [],
+      // Cleanup the interval when component unmounts or player switches
+      return () => clearInterval(interval);
+    }, [xIsNext]);
+
+    const isCurrentBoard = (idx: number) => {
+      if (winner) return false;
+
+      const { row, col } = lastMoveLocation;
+      if (row === null || col === null) {
+        return true;
+      }
+
+      const currentBoard = row * size + col;
+      return localWinners[currentBoard]
+        ? localWinners[idx] === null
+        : idx === currentBoard;
     };
 
-    for (let i = 0; i < size; i++) {
-      lines.row.push(x * size + i);
-      lines.col.push(i * size + y);
-    }
+    const handleClick = (inner_idx: number, outer_idx: number) => {
+      console.log(inner_idx, outer_idx);
 
-    if (x === y) {
-      for (let i = 0; i < size; i++) {
-        lines.diag.push(i * size + i);
+      if (
+        winner ||
+        !isCurrentBoard(outer_idx) ||
+        squares[outer_idx][inner_idx]
+      ) {
+        return;
       }
-    }
 
-    if (x + y === size - 1) {
-      for (let i = 0; i < size; i++) {
-        lines.antidiag.push(i * size + size - 1 - i);
+      const newSquares = squares.slice();
+      newSquares[outer_idx][inner_idx] = xIsNext ? "X" : "O";
+
+      setSquares(newSquares);
+
+      const lastMove = {
+        row: Math.floor(inner_idx / size),
+        col: inner_idx % size,
+        outerRow: Math.floor(outer_idx / size),
+        outerCol: outer_idx % size,
+      };
+
+      const newWinnerLine = calculateWinner(newSquares[outer_idx], lastMove);
+      const updatedWinners = localWinners.slice();
+      updatedWinners[outer_idx] =
+        newWinnerLine && newSquares[outer_idx][newWinnerLine[0]];
+      setLocalWinners(updatedWinners);
+
+      const globalWinnerLine = calculateWinner(updatedWinners, {
+        row: lastMove.outerRow,
+        col: lastMove.outerCol,
+      });
+      if (globalWinnerLine) {
+        setWinner(updatedWinners[globalWinnerLine[0]]);
       }
-    }
 
-    // Check values on each candidate line
-    for (let prop in lines) {
-      const line = lines[prop];
-      if (line.length !== size) continue;
-      const result = line.reduce(
-        (acc, index) => acc && squares[index] === lastPlayer,
-        true
+      setLastMoveLocation(lastMove);
+      setXIsNext(!xIsNext);
+    };
+
+    const calculateWinner = (
+      squares: any[],
+      lastMoveLocation: { row: number; col: number }
+    ) => {
+      if (!lastMoveLocation) return null;
+
+      const x = lastMoveLocation.row;
+      const y = lastMoveLocation.col;
+      const lastPlayer = squares[x * size + y];
+      if (lastPlayer === null) return null;
+
+      // Generate possible winner lines for the last move
+      var lines: { [key: string]: number[] } = {
+        row: [],
+        col: [],
+        diag: [],
+        antidiag: [],
+      };
+
+      for (let i = 0; i < size; i++) {
+        lines.row.push(x * size + i);
+        lines.col.push(i * size + y);
+      }
+
+      if (x === y) {
+        for (let i = 0; i < size; i++) {
+          lines.diag.push(i * size + i);
+        }
+      }
+
+      if (x + y === size - 1) {
+        for (let i = 0; i < size; i++) {
+          lines.antidiag.push(i * size + size - 1 - i);
+        }
+      }
+
+      // Check values on each candidate line
+      for (let prop in lines) {
+        const line = lines[prop];
+        if (line.length !== size) continue;
+        const result = line.reduce(
+          (acc, index) => acc && squares[index] === lastPlayer,
+          true
+        );
+        if (result) {
+          return line;
+        }
+      }
+      return null;
+    };
+
+    const shuffleBoards = () => {
+      const newOrder = Array.from([...Array(size * size).keys()]).sort(
+        () => Math.random() - 0.5
       );
-      if (result) {
-        return line;
+      const newSquares = newOrder.map((i) => squares[i]);
+      const newLocalWinners = newOrder.map((i) => localWinners[i]);
+
+      setSquares(newSquares);
+      setLocalWinners(newLocalWinners);
+
+      if (xIsNext) {
+        setPlayer1HasShuffled(true);
+      } else {
+        setPlayer2HasShuffled(true);
       }
-    }
-    return null;
-  };
+    };
 
-  return (
-    <Box className="game-container">
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="flex-start"
-        flexDirection="row"
-      >
-        <TurnIndicator player="X" isActive={xIsNext} timeLeft={player1Time} />
+    return (
+      <Box className="game-container">
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="flex-start"
+          flexDirection="row"
+        >
+          <TurnIndicator
+            player="X"
+            isActive={xIsNext}
+            timeLeft={player1Time}
+            gameMode={gameMode}
+            hasShuffled={player1HasShuffled}
+            onShuffle={shuffleBoards}
+          />
 
-        <Box className="game-grid" mx={5}>
-          {Array(size)
-            .fill(0)
-            .map((_, outerIdx) => (
-              <Box key={outerIdx} className="game-row">
-                {Array(size)
-                  .fill(0)
-                  .map((_, innerIdx) => {
-                    const boardIndex = outerIdx * size + innerIdx;
-                    return (
-                      <GameBoard
-                        key={boardIndex}
-                        squares={squares[boardIndex]}
-                        winner={localWinners[boardIndex] as any}
-                        clickable={isCurrentBoard(boardIndex)}
-                        onClick={(squareIndex) =>
-                          handleClick(squareIndex, boardIndex)
-                        }
-                      />
-                    );
-                  })}
-              </Box>
-            ))}
+          <Box className="game-grid" mx={8}>
+            {Array(size)
+              .fill(0)
+              .map((_, outerIdx) => (
+                <Box key={outerIdx} className="game-row">
+                  {Array(size)
+                    .fill(0)
+                    .map((_, innerIdx) => {
+                      const boardIndex = outerIdx * size + innerIdx;
+                      return (
+                        <GameBoard
+                          key={boardIndex}
+                          squares={squares[boardIndex]}
+                          winner={localWinners[boardIndex] as any}
+                          clickable={isCurrentBoard(boardIndex)}
+                          onClick={(squareIndex) =>
+                            handleClick(squareIndex, boardIndex)
+                          }
+                        />
+                      );
+                    })}
+                </Box>
+              ))}
+          </Box>
+          <TurnIndicator
+            player="O"
+            isActive={!xIsNext}
+            timeLeft={player2Time}
+            gameMode={gameMode}
+            hasShuffled={player2HasShuffled}
+            onShuffle={shuffleBoards}
+          />
         </Box>
-        <TurnIndicator player="O" isActive={!xIsNext} timeLeft={player2Time} />
       </Box>
-    </Box>
-  );
-});
+    );
+  }
+);
 
 export default Game;
