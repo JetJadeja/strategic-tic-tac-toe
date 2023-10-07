4fcf071f93cf4d08e529321eb33bae5ec8c9dc33
JetJadeja <JetJadeja@gmail.com>
2023-10-07 14:06:06 -0700

    Fix instructions

--- src/components/shared/Instructions.tsx
+++ src/components/shared/Instructions.tsx
@@ -9,6 +9,10 @@ import {
   List,
   ListItem,
   ListIcon,
+  Divider,
+  Spacer,
+  Icon,
+  Stack,
 } from "@chakra-ui/react";
 import {
   CheckCircleIcon,
@@ -20,64 +24,74 @@ import {
 const Instructions = React.memo(({ goBack }: { goBack: () => void }) => {
   return (
     <Fade in>
-      <VStack spacing={5} align="start" width="80%">
-        <Heading as="h2" size="2xl">
-          Instructions
+      <VStack spacing={6} align="start" p={5}>
+        <Heading as="h2" size="2xl" textAlign="center">
+          Gameplay Guide
         </Heading>
+        <Divider borderColor="gray.300" />
 
-        <Heading as="h3" size="lg" mb={2}>
-          <StarIcon boxSize="6" mr={2} /> Ultimate Tic Tac Toe:
-        </Heading>
-        <List spacing={2} mb={4}>
+        <Stack direction="row" align="center" spacing={3}>
+          <Icon as={StarIcon} boxSize="8" color="blue.500" />
+          <Heading as="h3" size="lg">
+            Ultimate Tic Tac Toe
+          </Heading>
+        </Stack>
+        <List spacing={2} pl={5}>
           <ListItem>
             <ListIcon as={CheckCircleIcon} color="green.500" />
-            Ultimate Tic Tac Toe is a 3x3 grid of standard 3x3 Tic Tac Toe
-            boards.
+            Comprised of 9 Tic Tac Toe boards arranged in a 3x3 grid.
           </ListItem>
           <ListItem>
             <ListIcon as={CheckCircleIcon} color="green.500" />
-            The player makes a move in any of the small boards.
+            Each move you make dictates where your opponent can play next.
           </ListItem>
           <ListItem>
             <ListIcon as={CheckCircleIcon} color="green.500" />
-            The next move by the opponent must be made in the board
-            corresponding to the cell of the previous move.
+            Aim to win the smaller boards, and consequently, control the larger
+            board.
           </ListItem>
           <ListItem>
             <ListIcon as={CheckCircleIcon} color="green.500" />
-            If a small board is won or is a draw, the next player can play
-            anywhere.
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="green.500" />
-            The game is won when a player wins three small boards in a row,
-            column, or diagonal.
+            Keep in mind the strategic significance of every move. Sometimes,
+            sacrificing a small board win can give an advantage on the main
+            board.
           </ListItem>
         </List>
 
-        <Heading as="h3" size="lg" mb={2}>
-          <RepeatIcon boxSize="6" mr={2} /> Crazy Mode:
-        </Heading>
-        <Text fontSize="md" mb={4}>
-          This is Ultimate Tic Tac Toe with a twist! Both players get a button
-          called "shuffle" that allows them to rearrange the 9 main squares in a
-          random order. This shuffle can only be used once by each player during
-          the game.
+        <Spacer height="20px" />
+
+        <Stack direction="row" align="center" spacing={3}>
+          <Icon as={RepeatIcon} boxSize="8" color="teal.500" />
+          <Heading as="h3" size="lg">
+            Crazy Mode
+          </Heading>
+        </Stack>
+        <Text fontSize="md" pl={5} fontWeight="medium">
+          Enhance your strategy in Ultimate Tic Tac Toe! Each player receives a
+          "shuffle" power. Pressing this button will rearrange the main 9
+          squares randomly. But beware! This can only be done once per game.
         </Text>
 
-        <Heading as="h3" size="lg" mb={2}>
-          <WarningIcon boxSize="6" mr={2} /> Battle Squares Mode:
-        </Heading>
-        <Text fontSize="md" mb={4}>
-          Similar to the standard game, but one square is highlighted in light
-          purple. The first player to win this square earns a "shuffle" button,
-          allowing them to rearrange the 9 main squares in a random order. This
-          power can only be used once.
+        <Spacer height="20px" />
+
+        <Stack direction="row" align="center" spacing={3}>
+          <Icon as={WarningIcon} boxSize="8" color="yellow.500" />
+          <Heading as="h3" size="lg">
+            Battle Squares Mode
+          </Heading>
+        </Stack>
+        <Text fontSize="md" pl={5} fontWeight="medium">
+          It's a race to power! A square will be highlighted in light purple.
+          The player who secures this square first is awarded the "shuffle"
+          power. With this, you can rearrange the main 9 squares in any random
+          sequence. This power is a one-time opportunity, so use it wisely.
         </Text>
 
-        <Box alignSelf="center">
+        <Spacer height="20px" />
+
+        <Box alignSelf="center" mt={5}>
           <Button colorScheme="teal" onClick={goBack}>
-            Back
+            Return
           </Button>
         </Box>
       </VStack>
